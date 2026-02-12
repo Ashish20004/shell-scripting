3b0e4892d483b2d8717b0067a309d1814db9edf0
Ashish <ashishkumaryandra@gmail.com>
2026-02-12 12:06:05 +0530

    new changes

--- 02.variables.sh
+++ 02.variables.sh
@@ -7,3 +7,11 @@ echo "Hello $name, nice to meet you!"
 echo "What is your age?"
 read age
 echo "You are $age years old."
+
+echo "Name of the script is:$0"
+echo "First argument is:$1"
+echo "second argument is:$2"
+echo "All arguments are:$@"
+echo "Number of arguments is:$#"
+echo "Exit status of last command is:$?"
+echo "process id of the script is:$$"
