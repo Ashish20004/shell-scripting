07cea9b056e86c5b8254832b9c7dca3d83d8d3f0
Ashish <ashishkumaryandra@gmail.com>
2026-02-12 11:48:56 +0530

    created another file

---
+++ 02.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "$PERSON : hi there"
+echo "What is your name?"
+read name
+echo "Hello $name, nice to meet you!"
+echo "What is your age?"
+read age
+echo "You are $age years old."
