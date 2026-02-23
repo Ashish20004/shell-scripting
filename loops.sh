7fdb5297d5232d9c2366239ff421c7d446a143c2
Ashish <ashishkumaryandra@gmail.com>
2026-02-23 20:03:49 +0530

    added new loops in a file

--- loops.sh
+++ loops.sh
@@ -1,7 +1,34 @@
   #!bin/bash
 
-for i in {1..100}
+# for i in {1..100}
+# do
+#     echo $i
+# done
+# git 
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
 do
-    echo $i
+    dnf install $package -y &>>$LOGS_FILE
+    VALIDATE $? "$package installation"
 done
-git 
