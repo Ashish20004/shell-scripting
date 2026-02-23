1856bbdb5c69e8649c644230739047be7e7b5362
Ashish <ashishkumaryandra@gmail.com>
2026-02-23 21:51:05 +0530

    colors in shell scripting

---
+++ color.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+echo -e "${R}This is Red color"
+echo -e "${G}This is Green color"
+echo -e "${Y}This is Yellow color"
+echo -e "${B}This is Blue color"    
--- loops02.sh
+++ loops02.sh
@@ -26,7 +26,7 @@ do
     if [ $? -ne 0 ]; then
         echo "$package not installed, installing now"
         dnf install $package -y &>>$LOGS_FILE
-        VALIDATE $? "$package installation"
+        VALIDATE $? "$package installation" 
     else
         echo "$package already installed, skipping"
     fi
