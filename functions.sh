f56619525ffa51021737844869f61f681e59301f
Ashish <ashishkumaryandra@gmail.com>
2026-02-23 18:22:07 +0530

    function file added

---
+++ functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u) #-u means uid of the user
+
+if [ $USERID -ne 0 ]; then 
+    echo "please run the script as root user";
+    exit 1;
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo "Installing.......$2 package id failed"
+    else
+        echo "Installing.......$2 package is success"
+    fi
+}
+
+dnf install mongo -y
+validate $? "Installing mongo"
+
+dnf install redis -y
+validate $? "Installing redis"
