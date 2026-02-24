19f91af5bd4b878934c69714ece675f6350eab3b
Ashish <ashishkumaryandra@gmail.com>
2026-02-24 10:58:40 +0530

    adding set file

---
+++ set.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e #set -e makes a shell script exit immediately when any command returns a non-zero status.
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi  
+mkdir -p $LOGS_FOLDER
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE$N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>>$LOGS_FILE
+       # validate $? "$package installation"
+    else
+        echo -e "$package already installed ... $R SKIPPING $N"
+    fi
+done
